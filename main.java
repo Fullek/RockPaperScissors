2519fe83ffbf89f110eebcad0ce918eecaa28c99
Fullek <62626590+Fullek@users.noreply.github.com>
2020-04-05 23:32:57 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Menus menus = new Menus();
+        RepeatProgram repeat = new RepeatProgram();
+        GetSet getSet = new GetSet();
+
+        System.out.println("Let's play Rock, Paper, Scissors game!");
+
+        do {
+            menus.header();
+            menus.checker();
+            repeat.continueOrNot();
+        } while (repeat.exit);
+    }
+}
