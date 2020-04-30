7a5719c7bfdd5d3d9bae7d55978fc55ea737ff3d
Fullek <62626590+Fullek@users.noreply.github.com>
2020-04-30 12:13:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Menus menus = new Menus();
-        RepeatProgram repeat = new RepeatProgram();
-
-        System.out.println("Let's play Rock, Paper, Scissors game!");
-
-        do {
-            menus.header();
-            menus.checker();
-            repeat.continueOrNot();
-        } while (repeat.exit);
+        GameController gameController = new GameController();
+        gameController.programDemo();
     }
 }
